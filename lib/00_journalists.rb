6cdf45641a08e9a4a1c8fe9d9c4e3aa26d78f466
valentin-orrit-wsl <vorrit@gmail.com>
2024-04-04 16:50:09 +0200

    finished exo except last one

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,53 +1,86 @@
 require_relative "./00b_journalists_db.rb"
 
-def handlesNum (array) 
+def stringsNum(array) 
     puts "-" * 10
     puts "there are #{array.length} journalists in the database."
 end
 
 
-def shortestHandle (array)
+def shorteststring(array)
     puts "-" * 10
-    min = array.min_by {|handle| handle.length}
+    min = array.min_by {|string| string.length}
 
     puts "the shortest handle is #{min}."
 end
 
-def fiveChar (array)
+
+def fiveChar(array)
     puts "-" * 10
-    newArray = array.map {|handle| handle.length}
+    newArray = array.map {|string| string.length}
     newArray.delete_if{|num| num > 4}
 
-    puts "there are #{newArray.length} handles with 5 characters"
+    puts "there are #{newArray.length} handle with 5 characters"
 end
 
 
 def startWithCap(array)
-    #array.select {|handle| ("A".."Z").include?(handle[1])}
     puts "-" * 10
-    newArray = []
-
-    array.each do |handle|
-        newArray.push(handle[1] =~ /[A-Z]/)
-    end 
+    newArray = array.map {|string| string[1] =~ /[A-Z]/}
     
     puts "there are #{newArray.count(0)} handles that start with a cap"
 end
-#epenser = 373
-
-def alphabeticalOrder (array)
 
 
+def alphabeticalSort(array)
+    puts "-" * 10
+    newArray = array.sort
+
+    puts newArray[0, 10].inspect
+end
+
+
+def sizeSort(array)
+    puts "-" * 10
+    newArray = array.sort_by {|string| string.length}
+
+    puts newArray[0, 10].inspect
 end
 
 
 
+def epenser(array)
+    puts "-" * 10
+    index = array.index("@epenser")
+
+    puts "@epenser is at the index: #{index}" 
+    puts "-" * 10
+end 
+
+# def sizeDistrib(array)
+#     puts "-" * 10
+#     #newArray = array.map {|string| string.length}
+#     newArray = array.sort_by {|string| string.length}
+
+#     newArray.each do |i|
+#         if 
+#             newArray[]
+#         end
+#     end 
+
+#     puts newArray.inspect
+# end
+
+# 3 char = 1 / 32 char = 1
+
 def output
-    #handlesNum($journalists)
-    #shortestHandle($journalists)
-    #fiveChar($journalists)
-    #startWithCap($journalists)
-    alphabeticalOrder($journalists)
+    stringsNum($journalists)
+    shorteststring($journalists)
+    fiveChar($journalists)
+    startWithCap($journalists)
+    alphabeticalSort($journalists)
+    sizeSort($journalists)
+    epenser($journalists)
+    #sizeDistrib($journalists)
 end
 
 output
